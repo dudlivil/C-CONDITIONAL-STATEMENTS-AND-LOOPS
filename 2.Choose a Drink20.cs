7832b2da4f628b3b299f8b2841135e33f7e1b978
dudlivil <32274101+dudlivil@users.noreply.github.com>
2017-09-28 18:54:12 +0300

    Initial upload

---
+++ 2.Choose a Drink20.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace ConsoleApplication49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var profession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            if (profession == "Athlete")
+            {
+                Console.WriteLine("The Athlete has to pay {0:f2}.", quantity * 0.70);
+            }
+            else if (profession == "Businessman" || profession == "Businesswoman")
+            {
+                Console.WriteLine("The " + profession + " has to pay {0:f2}.", quantity);
+            }
+            else if (profession == "SoftUni Student")
+            {
+                Console.WriteLine("The SoftUni Student has to pay {0:f2}.", quantity * 1.70);
+            }
+            else
+            {
+                Console.WriteLine("The " + profession + " has to pay {0:f2}.", quantity * 1.20);
+            }
+        }
+    }
+}
---
+++ 4.Hotel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nightsCount = int.Parse(Console.ReadLine());
+            if (month == "May")
+            {
+                if (nightsCount > 7)
+                {
+                    Console.WriteLine($"Studio: {nightsCount * 50 - 5.0 / 100 * nightsCount * 50:F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 65:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 75:F2} lv.");
+                }
+                else if (nightsCount <= 7)
+                {
+                    Console.WriteLine($"Studio: {nightsCount * 50:F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 65:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 75:F2} lv.");
+                }
+            }
+            else if (month == "October")
+            {
+                if (nightsCount > 7)
+                {
+                    Console.WriteLine($"Studio: {(nightsCount * 50 - 50) - 5.0 / 100 * (nightsCount * 50 - 50):F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 65:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 75:F2} lv.");
+                }
+                else if (nightsCount <= 7)
+                {
+                    Console.WriteLine($"Studio: {nightsCount * 50:F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 65:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 75:F2} lv.");
+                }
+            }
+            else if (month == "June" || month == "September")
+            {
+                if (nightsCount > 7 && month == "September")
+                {
+                    Console.WriteLine($"Studio: {nightsCount * 60 - 60:F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 72:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 82:F2} lv.");
+                }
+                else if (nightsCount > 14)
+                {
+                    Console.WriteLine($"Studio: {nightsCount * 60 - 60:F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 72 - 1.0 / 10 * nightsCount * 72:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 82:F2} lv.");
+                }
+                else if (nightsCount <= 7)
+                {
+                    Console.WriteLine($"Studio: {nightsCount * 60:F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 72:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 82:F2} lv.");
+                }
+            }
+            else if (month == "July" || month == "August" || month == "December")
+            {
+                if (nightsCount > 14)
+                {
+                    Console.WriteLine($"Studio: {nightsCount * 68:F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 77:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 89 - 15.0 / 100 * nightsCount * 89:F2} lv.");
+                }
+                else if (nightsCount <= 14)
+                {
+                    Console.WriteLine($"Studio: {nightsCount * 68:F2} lv.");
+                    Console.WriteLine($"Double: {nightsCount * 77:F2} lv.");
+                    Console.WriteLine($"Suite: {nightsCount * 89:F2} lv.");
+                }
+            }
+        }
+    }
+}
